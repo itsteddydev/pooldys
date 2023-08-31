848078eb845ef8060cc1fe63ef4e67efd1892c04
justcoddev <edisonpaulcrz@hotmail.com>
2023-08-30 20:36:31 -0500

    MF: Implementando Card Component

--- projects/products/src/app/pages/products/products.component.ts
+++ projects/products/src/app/pages/products/products.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { CardComponent } from './components/card/card.component';
+import { Observable } from 'rxjs';
+import { Product } from './interfaces/product.interface';
 
 @Component({
-  selector: 'app-products',
+  selector: 'pooldys-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
+  standalone: true,
+  imports: [
+    NgIf,
+    NgFor,
+    AsyncPipe,
+    CardComponent
+  ]
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  product$!: Observable<Product[]>;
+  private readonly productsSvc = inject(ProductsService);
+  // constructor() { }
 
   ngOnInit() {
+    this.product$ = this.productsSvc.getProducts();
   }
 
 }
